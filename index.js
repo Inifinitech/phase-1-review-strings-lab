1f85f2816891f1e734dacc189f372673bcc354bb
Izaack Kuria <kuriaisac@gmail.com>
2024-06-24 13:48:01 +0300

    Adds Variables and strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+// variable to define current user
+const currentUser = 'Kevin';
+// variable to define welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// variable to convert message to upperCase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
